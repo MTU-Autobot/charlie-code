dacefb32f3dcadd85904c0adacbf5e3cdebbca33
Nathan Duprey <nathanduprey@hotmail.com>
2016-11-03 19:34:27 -0400

    some basic stuff

--- src/meat.py
+++ src/meat.py
@@ -1,1 +1,19 @@
-import 
+import serial
+import time
+
+
+
+with serial.Serial('/dev/ttyACM1', 115200, timeout=1) as ser:
+    cmd = 2250
+
+    while 1:
+        ser.write((str(cmd) + '\n').encode())
+        line = ser.readline()
+        print(line.decode())
+
+        cmd += 10
+
+        if cmd > 4500:
+            break
+
+        time.sleep(0.1)
