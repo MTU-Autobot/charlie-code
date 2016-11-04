65c09f49c5255a14e01d0a7e4e4e49c5fcd93414
Nathan Duprey <nathanduprey@hotmail.com>
2016-11-03 20:36:17 -0400

    find correct serial port

--- src/meat.py
+++ src/meat.py
@@ -1,9 +1,21 @@
 import serial
+import serial.tools.list_ports
 import time
 
+TEENSY_VID = 0x16C0
+TEENSY_PID = 0x0483
 
+# find the correct serial port
+teensyPort = ""
+ports = serial.tools.list_ports.comports()
+for port in ports:
+    vid = port.vid
+    pid = port.pid
+    if vid == TEENSY_VID and pid == TEENSY_PID:
+        teensyPort = port.device
+        break
 
-with serial.Serial('/dev/ttyACM1', 115200, timeout=1) as ser:
+with serial.Serial(teensyPort, 115200, timeout=1) as ser:
     cmd = 2250
 
     while 1:
