56d96f9899833afb4fcb72869198114b3bd114c8
Haden Wasserbaech <spartanhaden@gmail.com>
2016-11-08 05:29:40 -0500

    Realized the IMU node refrences the library

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-    packages=['interfacer_package','imu_package'],
+    packages=['interfacer_package'],
     package_dir={'': 'src'},
 )
 
