9736a2ec897bcd60ce858bf5af97bfcd5e5a1156
ctjoyal <ctjoyal@mtu.edu>
2017-12-09 14:47:23 -0500

    Update waypoint_generator.py

--- temp/waypoint_generator.py
+++ temp/waypoint_generator.py
@@ -87,7 +87,7 @@ with open("gps_waypoints.txt") as f:
 	for line in f:
 		coordinateList.append(line.split(" ", 2))
 
-print "Coordinate List:"
+print("Coordinate List:")
 print str(coordinateList)
 
 while waypointCount < len(coordinateList):
@@ -108,9 +108,9 @@ while waypointCount < len(coordinateList):
 			continue
 		nextCoordinates[0] = float(coordinateList[waypointCount][0])
 		nextCoordinates[1] = float(coordinateList[waypointCount][1])
-		print "Current GPS Coordinates:"
+		print("Current GPS Coordinates:")
 		print str(currentCoordinates)
-		print "Target GPS Coordinates:"
+		print("Target GPS Coordinates:")
 		print str(nextCoordinates)
 		
 		# Calculate next waypoint. It will be a distance from Charlie's current location
@@ -120,7 +120,7 @@ while waypointCount < len(coordinateList):
 		temp = (nextWaypoint[1] * math.sin(currentOrientation)) + (nextWaypoint[0] * math.cos(currentOrientation))
 		nextWaypoint[1] = (nextWaypoint[1] * math.cos(currentOrientation)) - (nextWaypoint[0] * math.sin(currentOrientation))
 		nextWaypoint[0] = temp
-		print "Next Waypoint:"
+		print("Next Waypoint:")
 		print str(nextWaypoint)
 		
 		currentCoordinates[0] = nextCoordinates[0]
